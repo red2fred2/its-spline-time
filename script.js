a0f31e87250591d43ac7d22968ca53e3b7d886bb
red2fred2 <red2fred2base@live.com>
2022-08-26 02:43:03 -0500

    made Bezier roll over in his grave

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const T = 0.5
-
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 ctx.fillStyle = '#fff'
@@ -20,23 +18,53 @@ const line = (p1, p2) => {
 	ctx.stroke()
 }
 
-let points = [
+// I'm just assuming a and b are positive
+const lerp = (a, b, t) => a + t * (b - a)
+
+const pointLerp = (p1, p2, t) => {
+	return {
+		x: lerp(p1.x, p2.x, t),
+		y: lerp(p1.y, p2.y, t)
+	}
+}
+
+const drawPoints = (points) => {
+	for(p of points) point(p)
+}
+
+const drawLines = (pairs) => {
+	for(pair of pairs) line(pair.from, pair.to)
+}
+
+const getPairs = (points) => {
+	let pairs = []
+	for(let i = 0; i < points.length - 1; i++) pairs[i] = {from: points[i], to: points[i+1]}
+	return pairs
+}
+
+const lerpPairs = (pairs, t) => {
+	let p = []
+	for(pair of pairs) p.push(pointLerp(pair.from, pair.to, t))
+	return p
+}
+
+const bezier = (points, t) => {
+	let length = points.length
+	let pairs = []
+
+	for(let i = 0; i < length; i++) {
+		drawPoints(points)
+		pairs = getPairs(points)
+		drawLines(pairs)
+		points = lerpPairs(pairs, t)
+	}
+}
+
+// Actually do some stuff
+
+bezier([
 	{x: 100, y: 400},
 	{x: 150, y: 200},
 	{x: 350, y: 200},
 	{x: 450, y: 400},
-]
-
-// Draw points
-for(p of points) point(p)
-
-// Find pairs of points in order
-let pairs = []
-for(let i = 0; i < points.length - 1; i++) pairs[i] = {from: points[i], to: points[i+1]}
-
-// Draw lines
-for(pair of pairs) line(pair.from, pair.to)
-
-// Interpolate the first time
-let interpolation1points = []
-for(pair of pairs) interpolation1points.push()
+], 0.4)
